cd6a36873b7d8c0554035d698da765c34f1f7a60
Biinod81 <thomas.ly@iologo.io>
2022-03-27 18:32:15 +0200

    wip : about us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import { images } from '../../constants'
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt="g letter" />
+    </div>
+
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_aboutus'>
+        <h1 className='headtext__comorant'>About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+        <p className='p__opensans'>Lorem ipsum Dolor si aternt, Consecteteur Adipiscing Elit. Quis Pharmacis Adipiscing Ultricies Vulputate Posuere Tristique. In Sed Odio Nec Aliquet Eu Proin Mauris Et.</p>
+        <button type="button" className='custom__button'>Know More</button>
+      </div>
+
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__comorant'>Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+        <p className='p__opensans'>Lorem ipsum Dolor si aternt, Consecteteur Adipiscing Elit. Quis Pharmacis Adipiscing Ultricies Vulputate Posuere Tristique. In Sed Odio Nec Aliquet Eu Proin Mauris Et.</p>
+        <button type="button" className='custom__button'>Know More</button>
+      </div>
+    </div>
   </div>
 );
 
