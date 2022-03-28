a8bd0800b1d01da3b240adcec3fbc55e41d34f3b
Thomas LY <thomas.ly@iologo.io>
2022-03-28 21:19:39 +0200

    add: footer

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -6,13 +6,13 @@ const MenuItem = ({ title, price, tags }) => (
   <div className="app__menuItem">
     <div className="app__menuItem-head">
       <div className="app__menuItem-name">
-        <p class="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+        <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
       </div>
 
       <div className="app__menuItem-dash" />
 
       <div className="app__menuItem-price">
-        <p class="p__cormorant">{price}</p>
+        <p className="p__cormorant">{price}</p>
       </div>
     </div>
 
